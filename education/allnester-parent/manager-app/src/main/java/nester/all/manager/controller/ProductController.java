0e68e2e19c24c305276a168982aa9a07633908d3
Aleksey Nesterov <zubatka.forever@icloud.com>
2024-08-14 16:14:49 +0400

    added support ExceptionHandler + logic

--- education/allnester-parent/manager-app/src/main/java/nester/all/manager/controller/ProductController.java
+++ education/allnester-parent/manager-app/src/main/java/nester/all/manager/controller/ProductController.java
@@ -4,12 +4,18 @@
  */
 package nester.all.manager.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import nester.all.manager.controller.payload.UpdateProductPayload;
 import nester.all.manager.entity.Product;
 import nester.all.manager.service.ProductService;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,10 +32,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
     
     private final ProductService productService;
+    
+    private final MessageSource messageSource;
 
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") int productId) {
-        return this.productService.findProduct(productId).orElseThrow();
+        return this.productService.findProduct(productId).orElseThrow(() -> new NoSuchElementException("catalogue.errors.product.not_found"));
     }
 
     @GetMapping
@@ -53,5 +61,14 @@ public class ProductController {
         this.productService.deleteProduct(product.getId());
         return "redirect:/catalogue/products/list";
     }
-
+    
+    @ExceptionHandler(NoSuchElementException.class)
+    public String hundleNoSuchElementException(NoSuchElementException exception,
+            Model model, HttpServletResponse response, Locale locale){
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error",
+               this.messageSource.getMessage(exception.getMessage(),
+                       new Object[0], exception.getMessage(), locale));
+        return "errors/404";
+    }
 }
